ddd809f1645690bec434063cdf5c45b910bb63a3
picolino <webmaster.pico@gmail.com>
2020-10-21 22:15:19 +0300

    Add more tests infrastructure

---
+++ isdayoff.Tests/IsDayOffInMemoryCacheTests/IsDayOffInMemoryTestBase.cs
@@ -0,0 +1,16 @@
+﻿using isdayoff.Core.Cache;
+
+namespace isdayoff.Tests.IsDayOffInMemoryCacheTests
+{
+    internal class IsDayOffInMemoryTestBase : TestBase
+    {
+        protected IsDayOffInMemoryCache IsDayOffInMemoryCache { get; private set; }
+        
+        public override void Setup()
+        {
+            base.Setup();
+
+            IsDayOffInMemoryCache = new IsDayOffInMemoryCache();
+        }
+    }
+}
