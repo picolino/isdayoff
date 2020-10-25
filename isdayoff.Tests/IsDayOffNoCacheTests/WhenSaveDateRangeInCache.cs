49f4432682b1f66c9f34eecd154bc701f34c1bd9
picolino <webmaster.pico@gmail.com>
2020-10-25 20:37:11 +0300

    Add passed test to no cache implementation

---
+++ isdayoff.Tests/IsDayOffNoCacheTests/WhenSaveDateRangeInCache.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using isdayoff.Contract;
+using isdayoff.Tests.Extensions;
+using NUnit.Framework;
+
+namespace isdayoff.Tests.IsDayOffNoCacheTests
+{
+    internal class WhenSaveDateRangeInCache : IsDayOffNoCacheTestBase
+    {
+        [Test]
+        public void NothingHappens()
+        {
+            IsDayOffNoCache.SaveDateRangeInCache(04.08.Of(2020), 04.08.Of(2020), Country.Russia, new List<DayOffDateTime>());
+            
+            Assert.Pass();
+        }
+    }
+}
